edf00ccc5aee41b6668cfaa7ee295a30ddd54cbf
Mark Ferguson <a261004@fil.com>
2015-11-27 15:48:19 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+app.set('port', process.env.PORT || 1337);
+
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/now', function(request, response) {
+	var d = new Date();
+	console.log("Received Date request");
+	response.status(200).send({date: d});
+
+});
+
+app.get('/base64', function(request, response) {
+	console.log("Received base64 request");
+	var stringToEncode = request.query.s;
+	var base64EncodedString = new Buffer(stringToEncode, 'utf8').toString('base64');
+	response.status(200).send(base64EncodedString);
+});
