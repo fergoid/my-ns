1d8942f4490bfd108dc12dcc7b62c6d27ecae663
markferguson <mark.ferguson0@gmail.com>
2016-04-08 16:47:15 +0100

    updated by atom

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ http.createServer(app).listen(app.get('port'), function() {
 
 app.get('/now', function(request, response) {
 	var d = new Date();
-	console.log("Received Date request from Mark");
+	console.log("Received Date request from someone");
 	response.status(200).send({date: d});
 
 });
